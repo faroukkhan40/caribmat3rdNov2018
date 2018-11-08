bef3ad185320f63c94fb71e058730b98c3630590
fkhan <fkhan37tt@gmail.com>
2018-11-08 15:27:57 -0800

    committ 2

---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package CaribbeanMathAcademy
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area" tyle="border:2px solid blue;float:left;margin:2px;">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
